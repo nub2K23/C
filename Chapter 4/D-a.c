632445e49316baad1976e7b5629a605b0f4fd44a
nub2K23 <150941574+nub2K23@users.noreply.github.com>
2024-01-05 08:58:14 +0530

    Update D-a.c

--- Chapter 4/D-a.c
+++ Chapter 4/D-a.c
@@ -11,11 +11,11 @@ int main() {
     scanf ("%f %f %f", &s1, &s2, &s3 ) ;
     
     // Check for equilateral, isosceles, and scalene
-    if (s1 == s2 && s2 == s3)
+    if (s1 == s2 && s2 == s3) {
         printf ("The triangle is an equilateral triangle.\n") ;
-    else if (s1 == s2 || s2 == s3 || s1 == s3)
+    } else if (s1 == s2 || s2 == s3 || s1 == s3) {
         printf ("The triangle is an isosceles triangle.\n") ;
-    else
+    } else {
         printf ("The triangle is a scalene triangle.\n") ;
     
 
