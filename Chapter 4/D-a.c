fff52764dc63c2a05a06a179656e3b51ca80b8a1
nub2K23 <150941574+nub2K23@users.noreply.github.com>
2024-01-05 08:59:03 +0530

    Update D-a.c

--- Chapter 4/D-a.c
+++ Chapter 4/D-a.c
@@ -17,7 +17,7 @@ int main() {
         printf ("The triangle is an isosceles triangle.\n") ;
     } else {
         printf ("The triangle is a scalene triangle.\n") ;
-    
+    }
 
     // Check for right-angled triangle using Pythagorean theorem
     if (s1*s1 + s2*s2 == s3*s3 || s1*s1 + s3*s3 == s2*s2 || s2*s2 + s3*s3 == s1*s1) {
