c9a235a9fe3605afd4cbae9dc64657f247e11b52
nub2K23 <arbindrahul123@gmail.com>
2024-01-27 19:43:07 +0530

    new

---
+++ Chapter 15/Exercises/A.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str1[50], str2[50], str3[50], str4[50];
+    printf("Enter the string 'Alice in wonder land.'\n");
+    scanf("%s%s%s%s", str1, str2, str3, str4);
+    printf("%s %s %s %s", str1, str2, str3, str4);
+    return 0;
+}
