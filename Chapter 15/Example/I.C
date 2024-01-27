c9869fea1fda9259cd7b78332b87578a9e2f3a3b
nub2K23 <arbindrahul123@gmail.com>
2024-01-27 16:32:27 +0530

    strcmp() function

---
+++ Chapter 15/Example/I.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char string1[] = "Jerry", string2[] = "Ferry";
+    int i, j, k;
+    i = strcmp(string1, "Jerry");
+    j = strcmp(string1, string2);
+    k = strcmp(string1, "Jerry boy");
+    printf("%d %d %d", i, j, k);
+    return 0;
+}
