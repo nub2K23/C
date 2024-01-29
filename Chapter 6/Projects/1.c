52c83e513e3624500f8c7b24161300f09fde5eb9
nub2k23 <arbindrahul123@gmail.com>
2024-01-30 02:29:07 +0530

    new

--- Chapter 6/Projects/1.c
+++ Chapter 6/Projects/1.c
@@ -3,22 +3,19 @@
 
 #include <stdio.h>
 
-int main()
-{
+int main() {
     // Declare variables
     int i, num;
     printf("Enter a number: ");
     scanf("%d", &num);
-
-    for (i=2; i <= num - 1; i++)
-    {
-        if (num % i == 0)
-        {
+    for (i=2; i <= num - 1; i++) {
+        if (num % i == 0) {
             printf("Not a prime number.");
             break;
         }
     }
-    if (i == num)
+    if (i == num) {
         printf("Prime number.");
+    }
     return 0;
 }
