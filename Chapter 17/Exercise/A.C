f2deb9d6ee845a870e16f00ce5d513ddc47a5999
nub2k23 <arbindrahul123@gmail.com>
2024-01-30 22:38:33 +0530

    new

---
+++ Chapter 17/Exercise/A.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    struct student
+    {
+        int roll;
+        char name[30];
+        char dept[10];
+        char course[10];
+        int year;
+    };
+}
