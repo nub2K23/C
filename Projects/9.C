9cfbcdfe2ef8322e2c9e9541323f29835ae98a8d
nub2k23 <arbindrahul123@gmail.com>
2024-02-01 11:34:05 +0530

    Question 9

--- Projects/9.C
+++ Projects/9.C
@@ -1,25 +1,23 @@
 // Find factors of a number.
 #include <stdio.h>
-
 void factors(int);
-
-int main() {
+int main()
+{
     int num;
-
     printf("Enter the number: ");
     scanf("%d", &num);
-
     factors(num);
-
     return 0;
 }
 
-void factors(int n) {
+void factors(int n)
+{
     int i;
-
     printf("Factors of %d are: 1 ", n);
-    for (i = 2; i <= n; i++) {
-        if (n % i == 0) {
+    for (i = 2; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
             printf("%d ", i);
         }
     }
