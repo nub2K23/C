1c708610e095977c7fbfbea4c91998662cc13e01
nub2k23 <arbindrahul123@gmail.com>
2024-02-01 09:41:56 +0530

    Question 5

--- Projects/5.C
+++ Projects/5.C
@@ -1,26 +1,29 @@
 // Recurring program to find the sum of the given series.
 
 #include <stdio.h>
-
 void series_sum(int);
-
-int main() {
+int main()
+{
     int n;
-
     printf("Enter number of terms to find the sum of: ");
     scanf("%d", &n);
-    
     series_sum(n);
-
     return 0;
 }
 
-void series_sum(int n) {
-    float sum = 0;
-
-    for (float i = 1; i <= n; i++) {
-        sum = sum + (1 / i);
+void series_sum(int n)
+{
+    int sum, diff = 0, even = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            even += i;
+        }
+        else
+        {
+            diff += i;
+        }
     }
-
-    printf("Sum of series = %.2f.", sum);
+    printf("Sum of first '%d' terms of series = %d.\n", n, sum = diff - even);
 }
