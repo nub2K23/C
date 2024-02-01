1b1fb81fb7f026872b008957582669a6883cfbe0
nub2k23 <arbindrahul123@gmail.com>
2024-02-01 09:41:49 +0530

    Question 4

--- Projects/4.C
+++ Projects/4.C
@@ -2,21 +2,18 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int num, temp, rnum = 0;
-
     printf("Enter the number: ");
     scanf("%d", &num);
-
     int ognum = num;
-
-    while (num > 0) {
+    while (num > 0)
+    {
         temp = num % 10;
         rnum = rnum * 10 + temp;
         num /= 10;
     }
-
     printf("Original Number: %d\nReverse Number: %d\n", ognum, rnum);
-    
     return 0;
 }
