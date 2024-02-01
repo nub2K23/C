5dc3b0334bb247bd2c6c7f2ea98353077b3f7319
nub2k23 <arbindrahul123@gmail.com>
2024-02-01 09:42:13 +0530

    Question 8

--- Projects/8.C
+++ Projects/8.C
@@ -1,40 +1,41 @@
 // Find if a number is prime or not using a function.
 #include <stdio.h>
-
-void isPrime(int, int*);
-
-int main() {
+void isPrime(int, int *);
+int main()
+{
     int num;
     int count = 0;
-
     printf("Enter the number: ");
     scanf("%d", &num);
-
     isPrime(num, &count);
-    
-    if (!count) {
+    if (!count)
+    {
         printf("%d is a prime number.\n", num);
-    } else {
+    }
+    else
+    {
         printf("%d is not a prime number.\n", num);
     }
-
     printf("Prime numbers under 100 are: \n2\n");
-    for (int n = 3; n < 100; n++) {
+    for (int n = 3; n < 100; n++)
+    {
         count = 0;
         isPrime(n, &count);
-        if (!count) {
+        if (!count)
+        {
             printf("%d\n", n);
         }
     }
-
     return 0;
 }
 
-void isPrime(int n, int *c) {
+void isPrime(int n, int *c)
+{
     int i;
-
-    for (i = 2; i < n; i++) {
-        if (n % i == 0) {
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
             (*c)++;
         }
     }
