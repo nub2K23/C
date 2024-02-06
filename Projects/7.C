df6e5b93986ab9b5de72c56d868e0fdc7f99cbcb
nub2K23 <arbindrahul123@gmail.com>
2024-02-06 19:23:05 +0530

    udpate 7.C

--- Projects/7.C
+++ Projects/7.C
@@ -3,35 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 int isPalindrome(char[]);
-int main()
-{
+int main() {
     char input[100];
     printf("Enter a string: ");
     scanf("%s", input);
-    if (isPalindrome(input))
-    {
+    if (isPalindrome(input)) {
         printf("%s is a palindrome!\n", input);
-    }
-    else
-    {
+    } else {
         printf("%s is not a palindrome.\n", input);
     }
     return 0;
 }
 
 // Function to check if a string is a palindrome
-int isPalindrome(char *str)
-{
+int isPalindrome(char *str) {
     int left = 0;
     int right = strlen(str) - 1;
-    while (left < right)
-    {
-        if (str[left] != str[right])
-        {
-            return 0; // Not a palindrome
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return 0;   // Not a palindrome
         }
-        left++;
-        right--;
+        left++, right--;
     }
-    return 1; // Palindrome
+    return 1;           // Palindrome
 }
