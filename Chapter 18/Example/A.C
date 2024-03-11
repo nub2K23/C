9d91183879a1c7825db7c8342160b4cb6cdc309e
nub2K23 <arbindrahul123@gmail.com>
2024-03-11 14:46:04 +0530

    A.C

---
+++ Chapter 18/Example/A.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int weight = 63;
+
+  printf("weight is %d kg\n", weight);
+  printf("weight is %2d kg\n", weight); 
+  printf("weight is %3d kg\n", weight);
+  printf("weight is %4d kg\n", weight);
+  printf("weight is %6d kg\n", weight);  
+  printf("weight is %-6d kg\n", weight);
+  printf("weight is %1d kg\n", weight); 
+  return 0;
+}
