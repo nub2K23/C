4bed9507d0533fa98065c1e54d2c5176900fb71b
nub2k23 <arbindrahul123@gmail.com>
2024-03-17 11:32:12 +0530

    C.C

---
+++ Chapter 18/Example/C.C
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Maths teaches reasoning,\nnot Algebra & Geometry\n");
+    printf("Future of learning \r is remote\n");
+    printf("You\tmust\tbe\tcrazy\vto\thate\tthis\tbook.\n");
+    
+    return 0;
+}
